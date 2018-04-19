2fea2f1b2cc1a6a796831bb7192f203182b317e4
jp-reynolds <jpreynolds215@gmail.com>
2018-04-19 13:13:57 -0600

    hopefully done with heroku

--- models/index.js
+++ models/index.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/todo-lab");
 
 if (process.env.NODE_ENV == "production") {
   mongoose.connect(process.env.MLAB_URL)
 } else {
-  mongoose.connect("mongodb://localhost/whenpresident");
+  mongoose.connect("mongodb://localhost/todo-lab");
 }
 
 
